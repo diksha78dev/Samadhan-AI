0ca25b6723989eecc85ced36fceb809790aa7ca9
Diksha Dabhole <dikshadabhole78@gmail.com>
2026-04-04 20:21:10 +0530

    Add backend tests and frontend documentation

---
+++ src/test/java/com/samadhan/samadhan_ai/SamadhanAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samadhan.samadhan_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamadhanAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
